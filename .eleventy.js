b95dc79486af35b1ce92b40c531b8f144b050c72
Ryan <rcmaddox@gmail.com>
2025-09-02 13:49:47 -0500

    Added collections and footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,42 @@
 module.exports = function(eleventyConfig) {
-  // Passthrough copy for your CSS folder
+  // --- Passthrough Copies ---
+  // These are files that Eleventy will copy directly to your output folder (_site).
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy({ "src/img": "img" });
 
-  // Passthrough copy for an images folder, if you have one
-  // eleventyConfig.addPassthroughCopy("img");
+  // --- Collections ---
+  // We are now manually telling Eleventy how to build our collections.
+  // This is more explicit and can solve issues where the automatic
+  // collection generation fails.
 
+  // 1. Create a 'characters' collection
+  eleventyConfig.addCollection("characters", function(collectionApi) {
+    // getFilteredByGlob tells Eleventy to find all Markdown files
+    // in the 'src/characters' folder.
+    return collectionApi.getFilteredByGlob("./src/characters/**/*.md");
+  });
+
+  // 2. Create a 'players' collection
+  eleventyConfig.addCollection("players", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("./src/players/**/*.md");
+  });
+
+  // 3. Create an 'items' collection
+  eleventyConfig.addCollection("items", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("./src/items/**/*.md");
+  });
+
+
+  // --- Eleventy Configuration ---
   return {
     dir: {
       input: "src",
       includes: "_includes",
-      output: "_site", // This is the crucial line!
+      output: "_site",
     },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
   };
 };
+
---
+++ src/characters/gundren.md
@@ -0,0 +1,5 @@
+---
+title: "Gundren Rockseeker" 
+photo: "/img/characters/gundren.jpg" 
+---
+A stout dwarf with a prospector's spirit and a nose for treasure. Gundren is a friend and the employer who kicked off this entire adventure by hiring the party to escort his goods to Phandalin. He and his brothers have reportedly found the long-lost Wave Echo Cave.
